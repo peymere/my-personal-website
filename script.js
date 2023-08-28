2d14bb95b109a25aaf88c8de9e9eabba65ea40ad
Peyton Meredith <peymeredith@gmail.com>
2023-08-28 12:45:18 -0500

    filler content, google fonts, css design elements

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This is my first website!";
+//const h2 = document.createElement("h2");
+//h2.textContent = "This is my first website!";
 
-document.querySelector("body").appendChild(h2);
+//document.querySelector("body").appendChild(h2);
