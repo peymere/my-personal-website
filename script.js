fc2b3f50768120aec0c333be5ee54ad58509e47f
Peyton Meredith <peymeredith@gmail.com>
2023-09-04 17:17:13 -0500

    buttons and center books

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-//const h2 = document.createElement("h2");
-//h2.textContent = "This is my first website!";
-
-//document.querySelector("body").appendChild(h2);
-
-//document.addEventListener('DOMContentLoaded', () => {
-    //const images = document.getElementById('images');
-    
-    //const firstBookClone = images.children[0].cloneNode(true);
-    //const lastBookClone = images.children[images.children.length - 1].cloneNode(true);
-
-    //images.appendChild(firstBookClone);
-    //images.insertBefore(lastBookClone, images.children[0]);
-
-    //images.style.transitionDuration = "0.0s"
-    //images.style.transform = `translate(-350px)`
-//});
-
 const next = document.getElementById("next");
 const prev = document.getElementById("prev");
 const images = document.getElementById("images");
